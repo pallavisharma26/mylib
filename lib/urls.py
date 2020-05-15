f2d82bbc16486acf4c28af287d9dc865e7ba2155
shubhamkumar198 <39937550+shubhamkumar198@users.noreply.github.com>
2020-05-15 20:43:39 +0530

    v0.0.7

--- lib/urls.py
+++ lib/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home-page'),
+    path('', views.index, name='home_page'),
+    path('movies/', views.movies, name='movie_page'),
+    path('musics/', views.musics, name='music_page'),
+    path('books/', views.books, name='book_page'),
+    path('pictures/', views.pictures, name='picture_page'),
 ]
--- lib/views.py
+++ lib/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'lib/index.html')
 
+def movies(request):
+    return render(request,'lib/movie.html')
+
+def musics(request):
+    return render(request,'lib/music.html')
+
+def books(request):
+    return render(request,'lib/book.html')
+
+def pictures(request):
+    return render(request,'lib/picture.html')
+
